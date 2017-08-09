740d4047b8e22f465ec8724440527b7e76754866
Rails Udemy Course <khristinasar@gmail.com>
2017-08-09 18:25:40 +0000

    home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sprockets  //= It has to be underneath jquery, _ujs
+//= require bootstrap-sprockets  
 //= require turbolinks
 //= require_tree .
 
